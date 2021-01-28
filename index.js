deeb63d029aa1e981b12c0b6567a74499b1ec89c
Sophie Gu <sophieqgu@gmail.com>
2021-01-28 04:49:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 class Formatter {
   //add static methods here
   static capitalize(string) {
-    return string.slice(0, 1).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9-'/s]+/g, '');
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
   }
 
   static titleize(string) {
-    return string.split(" ").map( word => Formatter.capitalize(word)).join(" ");
+    let exceptions = 
+    return string.split(" ").map( word => {
+      Formatter.capitalize(word)).join(" ");
+    }
   }
 }
