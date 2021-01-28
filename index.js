241a77217d7c11883b863c7dc0b77bb46984bed7
Sophie Gu <sophieqgu@gmail.com>
2021-01-28 05:02:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let array = string.split(" ");
+    array.unshift(this.capitalize(array.shift()));
+    let result = array.map(word => {
+
+      if (exceptions.includes(word)) {
+        return word;
+      } else {
+        return Formatter.capitalize(word);
+      }
+    })
+    return result.join(" ");
+  }
 }
